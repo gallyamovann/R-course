f9cf69aa487caf5d4564369faeb82f66aa0c1dfc
Nelli Gallyamova <96379045+gallyamovann@users.noreply.github.com>
2022-10-30 02:36:38 +0300

    added task2

--- lab1/task2.R
+++ lab1/task2.R
@@ -3,5 +3,6 @@ get_id <- function(df) {
   result <- data.frame(id=new_df[, 1], mean=rowMeans(new_df[, -1]))
   return (result[complete.cases(result), ])
 }
+
 load("data/lab1_e2.Rdata")
 get_id(all_data)
