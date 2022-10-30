21f47d3a2454cd53f3bed590fb99e13fa9f11fc0
Nelli Gallyamova <96379045+gallyamovann@users.noreply.github.com>
2022-10-30 12:59:56 +0300

    fixed

--- lab1/task1.R
+++ lab1/task1.R
@@ -1,11 +1,9 @@
 fix_data <- function(df) {
   for (i in colnames(df)) {
     updated_string <- gsub(" ", "", df[[i]])
-    if (length(updated_string) > 0) {
-      for (j in 1:length(updated_string)) {
-        if (!is.na(as.numeric(updated_string[j]))) {
-          df[[i]][j] <- as.numeric(updated_string[j])
-        }
+    for (j in seq_along(updated_string)) {
+      if (!is.na(as.numeric(updated_string[j]))) {
+        df[[i]][j] <- as.numeric(updated_string[j])
       }
     }
   }
